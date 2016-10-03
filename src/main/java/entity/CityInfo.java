2562e8b695a3632dbf2804f88df8614fa6e0775f
emmablomsterberg <emmablomsterberg@10.50.137.232>
2016-10-03 10:58:22 +0200

    merge conflict

--- src/main/java/entity/CityInfo.java
+++ src/main/java/entity/CityInfo.java
@@ -18,7 +18,21 @@ public class CityInfo
 
     @Id
     private Long id;
+    
+    private int zipCode;
+    private String city;
 
+    public CityInfo()
+    {
+    }
+    
+    
+    public CityInfo(int zipCode, String city)
+    {
+        this.zipCode = zipCode;
+        this.city = city;
+    }   
+    
     public Long getId()
     {
         return id;
@@ -28,5 +42,24 @@ public class CityInfo
     {
         this.id = id;
     }
-    
+
+    public int getZipCode()
+    {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode)
+    {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity()
+    {
+        return city;
+    }
+
+    public void setCity(String city)
+    {
+        this.city = city;
+    }    
 }
