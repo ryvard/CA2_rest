fc1ab3ef80020102749955a5c72a4c5eda790b34
Mia Ryvard <cph-mr279@cphbusiness.dk>
2016-10-03 10:38:46 +0200

    Entity setup

---
+++ src/main/java/entity/Phone.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author miaryvard
+ */
+@Entity
+public class Phone
+{
+
+    @Id
+    private Long id;
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+    
+}
