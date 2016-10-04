7746383c3dc58b4baae75b11cbacdae618b60875
Mia Ryvard <cph-mr279@cphbusiness.dk>
2016-10-04 15:59:31 +0200

    ekstra commit

--- src/main/java/test/SchemaBuilder.java
+++ src/main/java/test/SchemaBuilder.java
@@ -11,19 +11,11 @@ public class SchemaBuilder {
     
     public static void main(String[] args) {
         
-//        
         HashMap<String, Object> puproperties = new HashMap();
-//        HashMap<String, Object> puproperties1 = new HashMap();
-//        
+      
         puproperties.put("javax.persistence.sql-load-script-source", "scripts/ClearDB.sql"); 
-//        puproperties.put("javax.persistence.sql-load-script-source", "scripts/populateZip.sql"); 
-//        puproperties.put("javax.persistence.sql-load-script-source", "scripts/Populate.sql");
-//        
-////        System.out.println(puproperties.get("javax.persistence.sql-load-script-source"));
-//
+       
         Persistence.generateSchema("PU_CA2", puproperties);
-//        //Persistence.generateSchema("PU_CA2", puproperties1);
-//        
         Persistence.generateSchema("PU_CA2", null);
         
     }
