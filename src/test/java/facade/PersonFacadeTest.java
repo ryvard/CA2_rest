533acc1f98771891ab48c3bacc902e97b7b19f71
emmablomsterberg <emmablomsterberg@10.50.137.232>
2016-10-04 12:22:20 +0200

    merge con

---
+++ src/test/java/facade/PersonFacadeTest.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade;
+
+import entity.Person;
+import java.util.HashMap;
+import java.util.List;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class PersonFacadeTest {
+    EntityManagerFactory emf;
+    PersonFacade fp;
+    
+    public PersonFacadeTest() {
+        fp = new PersonFacade();
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        System.out.println("Setup");
+        HashMap<String, Object> puproperties = new HashMap();
+        puproperties.put("javax.persistence.sql-load-script-source", "scripts/ClearDB.sql");
+        Persistence.generateSchema("restEx2", puproperties);
+        Persistence.generateSchema("restEx2", null);
+        fp.setEmf(Persistence.createEntityManagerFactory("restEx2"));
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getPerson method, of class PersonFacade.
+     */
+    @Test
+    public void testGetPerson() {
+        System.out.println("getPerson");
+        int id = 0;
+        PersonFacade instance = new PersonFacade();
+        Person expResult = null;
+        Person result = instance.getPerson(id);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getPersons method, of class PersonFacade.
+     */
+    @Test
+    public void testGetPersons_0args() {
+        System.out.println("getPersons");
+        PersonFacade instance = new PersonFacade();
+        List<Person> expResult = null;
+        List<Person> result = instance.getPersons();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getPersons method, of class PersonFacade.
+     */
+    @Test
+    public void testGetPersons_int() {
+        System.out.println("getPersons");
+        int zipCode = 0;
+        PersonFacade instance = new PersonFacade();
+        List<Person> expResult = null;
+        List<Person> result = instance.getPersons(zipCode);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
